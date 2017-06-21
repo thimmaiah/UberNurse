37d6fce6972ddf01a0383c86b201b89a4b67e828
thimmaiah <thimmaiah@gmail.com>
2017-06-21 21:07:58 +0530

    Fixed overtime algo

--- app/models/concerns/start_end_time_helper.rb
+++ app/models/concerns/start_end_time_helper.rb
@@ -16,51 +16,90 @@ module StartEndTimeHelper
   def night_shift_minutes
     night_shift_start = 0
     night_shift_end   = 0
+    minutes = nil
 
     logger.debug "self.start_date.hour = #{self.start_date.hour}, self.end_date.hour = #{self.end_date.hour}"
 
-    if( (self.start_date.hour <= 8 && self.end_date.hour <= 8) ||
-        (self.start_date.hour >= 20 && self.end_date.hour >= 20) ||
-        (self.start_date.hour >= 20 && self.end_date.hour <= 8))
-      night_shift_start = self.start_date
-      night_shift_end   = self.end_date
-      logger.debug "Condition 1"
+    if(self.start_date.day == self.end_date.day)
+      if( (self.start_date.hour <= 8 && self.end_date.hour <= 8) ||
+          (self.start_date.hour >= 20 && self.end_date.hour >= 20) ||
+          (self.start_date.hour >= 20 && self.end_date.hour <= 8))
+        night_shift_start = self.start_date
+        night_shift_end   = self.end_date
+        logger.debug "Condition 1"
 
-    elsif(self.start_date.hour <= 8 && self.end_date.hour >= 8)
-      night_shift_start = self.start_date
-      night_shift_end   = self.end_date.change({hour:8,min:0,sec:0})
-      logger.debug "Condition 2"
+      elsif(self.start_date.hour <= 8 && self.end_date.hour >= 8)
+        night_shift_start = self.start_date
+        night_shift_end   = self.end_date.change({hour:8,min:0,sec:0})
+        logger.debug "Condition 2"
 
-    elsif(self.start_date.hour >= 20 && self.end_date.hour >= 8)
-      night_shift_start = self.start_date
-      night_shift_end = self.end_date.change({hour:8,min:0,sec:0})
-      logger.debug "Condition 3"
+      elsif(self.start_date.hour >= 20 && self.end_date.hour >= 8)
+        night_shift_start = self.start_date
+        night_shift_end = self.end_date.change({hour:8,min:0,sec:0})
+        logger.debug "Condition 3"
 
-    elsif(self.start_date.hour <= 20 && self.end_date.hour <= 8)
-      night_shift_start = self.start_date.change({hour:20,min:0,sec:0})
-      night_shift_end   = self.end_date
-      logger.debug "Condition 4"
+      elsif(self.start_date.hour <= 20 && self.end_date.hour <= 8)
+        night_shift_start = self.start_date.change({hour:20,min:0,sec:0})
+        night_shift_end   = self.end_date
+        logger.debug "Condition 4"
 
-    elsif(self.start_date.hour <= 20 && self.end_date.hour >= 20)
-      night_shift_start = self.start_date.change({hour:20,min:0,sec:0})
-      night_shift_end = self.end_date
-      logger.debug "Condition 5"
+      elsif(self.start_date.hour <= 20 && self.end_date.hour >= 20)
+        night_shift_start = self.start_date.change({hour:20,min:0,sec:0})
+        night_shift_end = self.end_date
+        logger.debug "Condition 5"
 
-    elsif(self.start_date.hour >= 8 && self.end_date.hour <= 20 && self.start_date.day == self.end_date.day)
-      night_shift_start = 0
-      night_shift_end   = 0
-      logger.debug "Condition 6"
+      elsif(self.start_date.hour >= 8 && self.end_date.hour <= 20)
+        night_shift_start = 0
+        night_shift_end   = 0
+        logger.debug "Condition 6"
+      end
+    end
 
-    elsif(self.start_date.hour <= 20 && self.end_date.hour >= 8)
-      night_shift_start = self.start_date.change({hour:20,min:0,sec:0})
-      night_shift_end = self.end_date.change({hour:8,min:0,sec:0})
-      logger.debug "Condition 7"
+    if(self.start_date.day != self.end_date.day)
 
+      if(self.start_date.hour <= 20 && self.end_date.hour >= 8)
+        night_shift_start = self.start_date.change({hour:20,min:0,sec:0})
+        night_shift_end = self.end_date.change({hour:8,min:0,sec:0})
+        logger.debug "Condition 7"
+
+      elsif(self.start_date.hour >=8 && self.start_date.hour <= 20 && self.end_date.hour <= 8)
+        night_shift_start = self.start_date.change({hour:20,min:0,sec:0})
+        night_shift_end = self.end_date
+        logger.debug "Condition 8"
+
+      elsif(self.start_date.hour >= 20 && self.end_date.hour >= 8 && self.end_date.hour <= 20)
+        night_shift_start = self.start_date
+        night_shift_end = self.end_date.change({hour:8,min:0,sec:0})
+        logger.debug "Condition 9"
+
+      elsif(self.start_date.hour >= 20 && self.end_date.hour <= 8)
+        night_shift_start = self.start_date
+        night_shift_end = self.end_date
+        logger.debug "Condition 10"
+
+      elsif(self.start_date.hour <= 8 && self.end_date.hour <= 8)
+        # Here there are 2 separate night elements
+        early_morning_shift = (self.start_date.change({hour:8,min:0,sec:0}) - self.start_date).to_f / 60
+        late_night_shift = (self.end_date - self.start_date.change({hour:20,min:0,sec:0})).to_f / 60
+        
+        minutes = early_morning_shift + late_night_shift
+        logger.debug "Condition 11"
+
+      elsif(self.start_date.hour >= 20 && self.end_date.hour >= 20)
+        # Here there are 2 separate night elements
+        early_morning_shift = (self.end_date.change({hour:8,min:0,sec:0}) - self.start_date).to_f / 60
+        late_night_shift = (self.end_date - self.end_date.change({hour:20,min:0,sec:0})).to_f / 60
+        
+        minutes = early_morning_shift + late_night_shift
+        logger.debug "Condition 12"
+
+      end
     end
 
     logger.debug "night_shift_end = #{night_shift_end}, night_shift_start = #{night_shift_start}"
 
-    minutes = ((night_shift_end - night_shift_start).to_f / 60).round(0).to_f
+    minutes = ((night_shift_end - night_shift_start).to_f / 60) if minutes == nil # Hack for Condition 10
+    minutes = minutes.round(0).to_f
     logger.debug "minutes = #{minutes}"
 
     return minutes
