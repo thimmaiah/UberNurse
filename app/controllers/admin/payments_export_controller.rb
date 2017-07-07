9a724efd1d3bd07422c95801cde5b473efa6db49
thimmaiah <thimmaiah@gmail.com>
2017-07-07 22:07:34 +0530

    Better payment export formats

--- app/controllers/admin/payments_export_controller.rb
+++ app/controllers/admin/payments_export_controller.rb
@@ -3,7 +3,7 @@ module Admin
   class PaymentsExportController < Admin::ApplicationController
 
     before_action :authenticate_user!
-    
+
     def index
       @payments = Payment.all.includes(:user, :care_home, :staffing_request)
 
@@ -23,7 +23,18 @@ module Admin
         @payments = @payments.where("created_at <= ?", params[:created_at_end])
       end
 
-      render xls: "index"
+
+      case params[:report_format]
+      when "Carer"
+        template ="carer"
+      when "Care Home"
+        template = "care_home"
+      when "Both"
+        template = "both"
+      end
+
+      logger.info "################## #{params[:report_format]}, #{template}"
+      render template: "admin/payments_export/#{template}"
     end
   end
 
