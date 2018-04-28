f329edb672781a86602e97cc6dbe3262c1c4f13e
thimmaiah <thimmaiah@gmail.com>
2018-04-28 21:46:52 +0530

    Capistrano staging setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,9 +9,6 @@ Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
- 
-#server 'dev.connuct.co.uk', user: "ubuntu", roles: [:web, :app, :db], primary: true
-server 'prod.connuct.co.uk', user: "ubuntu", roles: [:web, :app, :db], primary: true
 
 set :rails_env, fetch(:stage)
 
