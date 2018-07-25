840388c24e5a044f0ea383ef0273e77388b816af
thimmaiah <thimmaiah@gmail.com>
2018-07-25 18:18:32 +0530

    TEsting of incentives complete

--- app/jobs/generate_incentives_job.rb
+++ app/jobs/generate_incentives_job.rb
@@ -4,7 +4,12 @@ class GenerateIncentivesJob < ApplicationJob
   def perform()
 
   	# We need to generate the incentives for the prev month
-  	prev_month_start = Date.today.beginning_of_month.prev_month
+    if Rails.env == "test"
+      # For tests its hard to generate prev month shifts, so we do this month
+      prev_month_start = Date.today.beginning_of_month
+    else
+  	 prev_month_start = Date.today.beginning_of_month.prev_month
+    end
   	Rails.logger.debug "GenerateIncentivesJob: Generating incentives for month starting #{prev_month_start}"
   	# Find all the closed shifts for the prev month
   	prev_month_closed_shifts = Shift.month_closed_shifts(prev_month_start)
