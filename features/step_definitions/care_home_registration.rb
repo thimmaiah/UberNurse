e89ac382d039a1b17b98e588b4fade9a92fc180f
Mohith Thimmaiah <thimmaiah@gmail.com>
2019-08-22 19:32:11 +0530

    Better testing for care home additional fields

--- features/step_definitions/care_home_registration.rb
+++ features/step_definitions/care_home_registration.rb
@@ -29,6 +29,14 @@ Then(/^the care home should be created$/) do
   last.postcode.should == @care_home.postcode
   last.phone.should == @care_home.phone
   last.image_url.should == @care_home.image_url  
+  last.vat_number.should == @care_home.vat_number
+  last.company_registration_number.should == @care_home.company_registration_number
+
+  last.parking_available.should == @care_home.parking_available
+  last.meals_provided_on_shift.should == @care_home.meals_provided_on_shift
+  last.meals_subsidised.should == @care_home.meals_subsidised
+  last.po_req_for_invoice.should == @care_home.po_req_for_invoice
+  
 end
 
 Then(/^the care home should be unverified$/) do
@@ -58,5 +66,20 @@ end
 
 When(/^When I submit the care homes registration page with "([^"]*)"$/) do |arg1|
   sleep(2)
+
+  fields = ["vat_number", "company_registration_number"]
+  fields.each do |k|
+    fill_in(k, with: @care_home[k], fill_options: { clear: :backspace })
+    sleep(1)
+  end
+
+  ionic_select(@care_home.carer_break_mins, "carer_break_mins", true)
+  ionic_select(@care_home.paid_unpaid_breaks, "paid_unpaid_breaks", true)
+
+  find("#parking_available").click if @care_home.parking_available
+  find("#meals_provided_on_shift").click if @care_home.meals_provided_on_shift
+  find("#meals_subsidised").click if @care_home.meals_subsidised
+  find("#po_req_for_invoice").click if @care_home.po_req_for_invoice
+
   click_on("Save")
 end
