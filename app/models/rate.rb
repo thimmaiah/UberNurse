d6b1cdd62247b7c5b93ce1388565ce194d9c9714
Mohith Thimmaiah <thimmaiah@gmail.com>
2019-10-14 18:40:47 +0530

    Better validation for non empty rates

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,6 +1,8 @@
 class Rate < ApplicationRecord
 
-  validates_presence_of :agency_id	
+  validates_presence_of :agency_id, :carer_weekday, :care_home_weekday, :carer_weeknight, :care_home_weeknight, 
+  						:carer_weekend, :care_home_weekend, :carer_weekend_night, :care_home_weekend_night, 
+  						:carer_bank_holiday, :care_home_bank_holiday
   
   belongs_to :agency
   belongs_to :care_home
